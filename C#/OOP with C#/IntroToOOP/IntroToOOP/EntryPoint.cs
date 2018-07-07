b26af6d262f407bf65fdab146c0109e49397b5d5
ramya15 <ramya.mjr@gmail.com>
2018-07-08 01:04:13 +0530

    Initial Commit

---
+++ C#/OOP with C#/IntroToOOP/IntroToOOP/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        Point point = new Point
+        {
+            x = 5,
+            y = 10
+        };
+
+        System.Console.WriteLine(point.x);
+        System.Console.WriteLine(point.y);
+
+        Human human = new Human(10, 20);
+        System.Console.WriteLine(human.Height);
+        System.Console.WriteLine(human.Weight);
+    }
+}
+
+//Constructor Demo
+class Human
+{
+    public double Height;
+    public double Weight;
+
+    //Constructor
+    public Human(double height, double weight)
+    {
+        Height = height;
+        Weight = weight;
+    }
+}
