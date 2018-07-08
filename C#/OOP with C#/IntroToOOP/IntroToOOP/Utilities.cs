328374dc2837e69e7ccc9390d3e83083d2f3068d
ramya15 <ramya.mjr@gmail.com>
2018-07-08 20:39:29 +0530

    ResetColor()

--- C#/OOP with C#/IntroToOOP/IntroToOOP/Utilities.cs
+++ C#/OOP with C#/IntroToOOP/IntroToOOP/Utilities.cs
@@ -2,13 +2,15 @@
 
 namespace IntroToOOP
 {
-    //Static Class (Singleton Design Pattern)
+    //Static Class =: equivalent of Singleton Design Pattern
+    //Static class cannot have a constructor as they cannot be instantited (as they do not need one)
     public static class Utilities
     {
         public static void ColorWriteLine(string message, ConsoleColor color)
         {
             Console.ForegroundColor = color;
             Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
